3555a7daaa8ca0967d64161f4da02dcf9580f23a
Sachin Biradar <84908560+Bsachin950@users.noreply.github.com>
2024-01-04 20:05:40 +0530

    New One

--- src/main/java/com/driver/controllers/AirportController.java
+++ src/main/java/com/driver/controllers/AirportController.java
@@ -1,75 +1,41 @@
 package com.driver.controllers;
 
 
-import com.driver.service.TripService;
 import com.driver.model.Airport;
 import com.driver.model.City;
 import com.driver.model.Flight;
 import com.driver.model.Passenger;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class AirportController {
-    private TripService tripService = new TripService();
+
+    AirportService servObj=new AirportService();
     @PostMapping("/add_airport")
     public String addAirport(@RequestBody Airport airport){
 
         //Simply add airport details to your database
         //Return a String message "SUCCESS"
-        tripService.addAirPort(airport);
+        servObj.addAirport(airport);
         return "SUCCESS";
-    }
-
-    @PostMapping("/add-passenger")
-    public String addPassenger(@RequestBody Passenger passenger)
-    {
-
-        //Add a passenger to the database
-        //And return a "SUCCESS" message if the passenger has been added successfully.
-
-        return tripService.addPassenger(passenger);
-    }
-
-    @PostMapping("/add-flight")
-    public String addFlight(@RequestBody Flight flight)
-    {
-
-        //Return a "SUCCESS" message string after adding a flight.
-        return tripService.addFlight(flight);
-    }
-
-    @PostMapping("/book-a-ticket")
-    public String bookATicket(@RequestParam("flightId") Integer flightId, @RequestParam("passengerId") Integer passengerId)
-    {
-
-        //If the numberOfPassengers who have booked the flight is greater than : maxCapacity, in that case :
-        //return a String "FAILURE"
-        //Also if the passenger has already booked a flight then also return "FAILURE".
-        //else if you are able to book a ticket then return "SUCCESS"
-
-        return tripService.bookATicket(flightId,passengerId);
-    }
-
-    @PutMapping("/cancel-a-ticket")
-    public String cancelATicket(@RequestParam("flightId")Integer flightId,@RequestParam("passengerId")Integer passengerId){
-
-        //If the passenger has not booked a ticket for that flight or the flightId is invalid or in any other failure case
-        // then return a "FAILURE" message
-        // Otherwise return a "SUCCESS" message
-        // and also cancel the ticket that passenger had booked earlier on the given flightId
-
-        return tripService.cancelATicket(flightId,passengerId);
     }
 
     @GetMapping("/get-largest-aiport")
     public String getLargestAirportName(){
 
+        String ans=servObj.getLargestAirportName();
         //Largest airport is in terms of terminals. 3 terminal airport is larger than 2 terminal airport
         //Incase of a tie return the Lexicographically smallest airportName
 
-        return tripService.getLargestAirPort();
+        return ans;
     }
 
     @GetMapping("/get-shortest-time-travel-between-cities")
@@ -77,18 +43,21 @@ public class AirportController {
 
         //Find the duration by finding the shortest flight that connects these 2 cities directly
         //If there is no direct flight between 2 cities return -1.
+        double temp=servObj.getShortestDurationOfPossibleBetweenTwoCities(fromCity,toCity);
 
-        return tripService.getShortestDurationOfPossibleBetweenTwoCities(fromCity,toCity);
+
+
+        return temp;
     }
 
     @GetMapping("/get-number-of-people-on-airport-on/{date}")
-    public int getNumberOfPeopleOn(@PathVariable("date") Date date,@RequestParam("airportName")String airportName)
-    {
+    public int getNumberOfPeopleOn(@PathVariable("date") Date date,@RequestParam("airportName")String airportName){
 
         //Calculate the total number of people who have flights on that day on a particular airport
-        //This includes both the people who have come for a flight and who have landed on an airport after their flight
 
-        return tripService.getNumberOfPeopleOn(date,airportName);
+        //This includes both the people who have come for a flight and who have landed on an airport after their flight
+        int ans=servObj.getNumberOfPeopleOn(date,airportName);
+        return ans;
     }
 
     @GetMapping("/calculate-fare")
@@ -98,18 +67,52 @@ public class AirportController {
         //Price for any flight will be : 3000 + noOfPeopleWhoHaveAlreadyBooked*50
         //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
         //This will not include the current person who is trying to book, he might also be just checking price
-
-        return tripService.calculateFare(flightId);
+        int fare=servObj.calculateFlightFare(flightId);
+        return fare;
 
     }
 
+
+    @PostMapping("/book-a-ticket")
+    public String bookATicket(@RequestParam("flightId")Integer flightId,@RequestParam("passengerId")Integer passengerId){
+
+        //If the numberOfPassengers who have booked the flight is greater than : maxCapacity, in that case :
+        //return a String "FAILURE"
+        //Also if the passenger has already booked a flight then also return "FAILURE".
+        //else if you are able to book a ticket then return "SUCCESS"
+        String ans=servObj.bookATicket(flightId,passengerId);
+
+        return ans;
+    }
+
+    @PutMapping("/cancel-a-ticket")
+    public String cancelATicket(@RequestParam("flightId")Integer flightId,@RequestParam("passengerId")Integer passengerId){
+
+        //If the passenger has not booked a ticket for that flight or the flightId is invalid or in any other failure case
+        // then return a "FAILURE" message
+        // Otherwise return a "SUCCESS" message
+        // and also cancel the ticket that passenger had booked earlier on the given flightId
+        String ans=servObj.cancelATicket(flightId,passengerId);
+        return ans;
+    }
 
 
     @GetMapping("/get-count-of-bookings-done-by-a-passenger/{passengerId}")
     public int countOfBookingsDoneByPassengerAllCombined(@PathVariable("passengerId")Integer passengerId){
 
         //Tell the count of flight bookings done by a passenger: This will tell the total count of flight bookings done by a passenger :
-        return tripService.countOfBookingsDoneByPassengerAllCombined(passengerId);
+        int ans=servObj.countOfBookingsDoneByPassengerAllCombined(passengerId);
+
+        return ans;
+
+    }
+
+    @PostMapping("/add-flight")
+    public String addFlight(@RequestBody Flight flight){
+
+        servObj.addFlight(flight);
+        //Return a "SUCCESS" message string after adding a flight.
+        return "SUCCESS";
     }
 
 
@@ -118,9 +121,9 @@ public class AirportController {
 
         //We need to get the starting airportName from where the flight will be taking off (Hint think of City variable if that can be of some use)
         //return null incase the flightId is invalid or you are not able to find the airportName
+        String ans=servObj.getAirportNameFromFlightId(flightId);
 
-
-        return tripService.getAirportNameFromFlightId(flightId);
+        return ans;
     }
 
 
@@ -130,9 +133,20 @@ public class AirportController {
         //Calculate the total revenue that a flight could have
         //That is of all the passengers that have booked a flight till now and then calculate the revenue
         //Revenue will also decrease if some passenger cancels the flight
+        int revenue=servObj.calculateRevenueOfAFlight(flightId);
 
-
-
-        return tripService.calculateRevenueOfAFlight(flightId);
+        return revenue;
     }
+
+
+    @PostMapping("/add-passenger")
+    public String addPassenger(@RequestBody Passenger passenger){
+
+        //Add a passenger to the database
+        //And return a "SUCCESS" message if the passenger has been added successfully.
+        servObj.addPassenger(passenger);
+        return "SUCCESS";
+    }
+
+
 }
